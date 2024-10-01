7e8e39ff388e434eeafd46a7133850e583cc988d
Ari <164577995+skibidischool@users.noreply.github.com>
2024-10-01 00:01:22 +0000

    test

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 echo Downloading $(head -n 1 link.txt)...
 
 if [ "$1" = true ]; then
-  wget -r -e robots=off --convert-links $(head -n 1 link.txt)
+  wget -r -e robots=off --convert-links $(head -n 1 link.txt) >> log.txt
 else
-  wget -e robots=off --convert-links $(head -n 1 link.txt)
+  wget -e robots=off --convert-links $(head -n 1 link.txt) >> log.txt
 fi
